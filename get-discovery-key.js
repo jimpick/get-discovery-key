db2b79d8b632b10b8aef1cd75fcb39db5099c868
Jim Pick <jim@backblend.com>
2018-04-01 12:49:29 -0700

    Fix conversion, add dns-discovery invocations
    For debugging.

--- get-discovery-key.js
+++ get-discovery-key.js
@@ -3,9 +3,23 @@
 const crypto = require('hypercore/lib/crypto')
 const toBuffer = require('to-buffer')
 
-const key = process.argv[2]
+let key = process.argv[2]
 if (!key) {
   console.error('Need a key!')
   process.exit(1)
 }
-console.log(crypto.discoveryKey(toBuffer(key)).toString('hex'))
+key = toBuffer(key, 'hex')
+const dk = crypto.discoveryKey(key)
+const dnsDk = dk.slice(0, 20)
+console.log('          Key:', key.toString('hex'))
+console.log('Discovery Key:', dk.toString('hex'))
+console.log(
+  ' DNS lookup 1: ' +
+  'dns-discovery lookup --server discovery1.publicbits.org ' +
+  dnsDk.toString('hex')
+)
+console.log(
+  ' DNS lookup 2: ' +
+  'dns-discovery lookup --server discovery2.publicbits.org ' +
+  dnsDk.toString('hex')
+)
